e75a2d4c3b099225396e1e91c85cd00ee20e9457
Gurpreet Singh <gurpreetchandiwala@gmail.com>
2025-05-31 08:54:09 +0000

    typeconverion

---
+++ 01-basics/03conversionoprations.js
@@ -0,0 +1,20 @@
+let score = "33"
+//use type conversion to convert string value in numaric
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// type conversion
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isRead = 1
+let = booleanIsReadIn = Boolean(isRead)
+console.log(booleanIsReadIn);
+
+let num = 44
+let srtingInnum = String(num)
+console.log(srtingInnum);
+console.log(typeof srtingInnum);
+
+
