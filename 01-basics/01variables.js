f2552d5f705578c0747e35ce992ea4a8f56425b9
Gurpreet Singh <gurpreetchandiwala@gmail.com>
2025-05-31 08:37:04 +0000

    vaiabels and datatype

---
+++ 01-basics/01variables.js
@@ -0,0 +1,38 @@
+const accountId = 123442
+let accountEmail = "gurpreet@google.com"
+var accountPassword = "1234"
+//accountCity = "jaipur"
+
+//accountId = 2 // not allowd
+
+accountEmail = "hc@dfdkf"
+accountPassword = "2343321"
+//accountCity = "bombay"
+
+let accountState;
+
+/*
+prefer not to use var
+becouse of issue in block scope and functional sc
+
+*/
+
+console.table([accountId, accountEmail, accountPassword , accountState])
+
+
+// just for satisfaction
+/*
+a = 3023;
+b = 34;
+c = 322;
+if(a >b && a > c){
+    console.log("a is greater");
+    
+} else if(b > a && b > c){
+    console.log("b is greater");
+    
+} else{
+    console.log("c is greater");
+    
+}*/
+
---
+++ 01-basics/02datatype.js
@@ -0,0 +1,13 @@
+let num = 1233
+let string = "guri"
+let isRead = true;
+let state; //undifindend value
+
+//number => 2 to 53
+//bigint
+//string => ""
+//boolean => true /false
+//undefined => 
+// symbol => unique
+
+
